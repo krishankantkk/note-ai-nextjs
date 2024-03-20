1bfcee28484e36b43486cd0dc98f4241455c023d
krishan kant <kantk1508@gmail.com>
2024-03-20 11:32:49 +0530

    dark/light theme

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -7,23 +7,33 @@ import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 import AddNoteDialog from "@/components/AddNoteDialog";
-
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 export default function NavBar() {
+  const { theme } = useTheme();
   const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
   return (
     <>
-      <div className="p-4 text-black shadow">
+      <div className="p-4 shadow">
         <div className="item-center m-auto flex max-w-7xl flex-wrap justify-between">
           <Link href="/notes" className="flex items-center gap-1">
-            <Image src={logo} alt="Smart note" width={40} height={40} />
+            <Image src={logo} alt="Smart note logo" width={40} height={40} />
             <span className="font-bold">Smart Note</span>
           </Link>
           <div className="flex items-center gap-2">
+            <ThemeToggleButton />
             <Button onClick={() => setShowAddNoteDialog(true)}>
               <Plus size={20} className="mr-2" />
               Add Note
             </Button>
-            <UserButton afterSignOutUrl="/" />
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
           </div>
         </div>
       </div>
